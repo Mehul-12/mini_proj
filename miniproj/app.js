ec5805e3d8e3c112cb8d56d18c79f4f7c6a19920
ankit4284 <anktigupta2000in@gmail.com>
2022-11-26 01:18:41 +0530

    script run

--- miniproj/app.js
+++ miniproj/app.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+
+const fse = require('fs-extra');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -13,6 +15,8 @@ const userRoutes = require('./routes/users');
 const {spawn} = require('child_process');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
+const { isLoggedIn} = require('./middleware');
+const User = require('./models/user');
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -36,12 +40,14 @@ app.use('/', userRoutes);
 app.get('/',(req,res)=>{
     res.send("Hi")
 });
-app.get('/xyz', (req, res) => {
+app.get('/xyz',isLoggedIn, (req, res) => {
   const { runstr } = req.body;
+  const userid=req.user._id;
   try{var dataToSend;
       var args= ['main.py'];
       var arr = runstr.split(" ");
       arg=args.concat(arr);
+      let namefile=userid+"-"+Date.now().toString();
       // console.log(args);
       // res.send("hello");
       // ['main.py','bnr', 'lgr', 'saga', 'l1', '3']
@@ -53,6 +59,30 @@ app.get('/xyz', (req, res) => {
       });
       python.on('close', (code) => {
         console.log(`child process close all stdio with code ${code}`);
+        let src1 = "test_clf_rep.png";
+        let dest1 = `uploads/${namefile}_test_clf_rep.png`;
+        fse.move(src1, dest1, (err) => {
+          if (err) return console.log(err);
+          console.log(`File successfully moved!!`);
+        });
+        let src2 = "test_conf_mat.png";
+        let dest2 = `uploads/${namefile}_test_conf_mat.png`;
+        fse.move(src2, dest2, (err) => {
+          if (err) return console.log(err);
+          console.log(`File successfully moved!!`);
+        });
+        let src3 = "training_clf_rep.png";
+        let dest3 = `uploads/${namefile}_training_clf_rep.png`;
+        fse.move(src3, dest3, (err) => {
+          if (err) return console.log(err);
+          console.log(`File successfully moved!!`);
+        });
+        let src4 = "training_conf_mat.png";
+        let dest4 = `uploads/${namefile}_training_conf_mat.png`;
+        fse.move(src4, dest4, (err) => {
+          if (err) return console.log(err);
+          console.log(`File successfully moved!!`);
+        });
         res.send(dataToSend)
       });
   }
